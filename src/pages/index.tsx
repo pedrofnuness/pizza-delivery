0959c066ba192ab25077df119efb3a72e2d003bc
Pedro <pedro.nunes@southsystem.com.br>
2021-04-18 16:08:41 -0300

    :sparkles: feat: create button

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import DoughStep from "../components/doughStepComponent";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>iPizza Delivery</h1>
+
+      <DoughStep />
+
+      <button className={styles.button}>Selecionar</button>
+
+      <footer>
+        <p>
+          <i>iPizza</i> © Todos os direitos reservados
+        </p>
+      </footer>
+    </div>
+  );
+}
