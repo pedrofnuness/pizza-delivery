37a6a549da6e09cf9390fb1887f1708ad200e4a9
Pedro <pedro.nunes@southsystem.com.br>
2021-04-19 01:42:28 -0300

    :sparkles: feat: api implementation to get the pizza of the day

--- frontend/src/components/Recommendation/Recommendation.tsx
+++ frontend/src/components/Recommendation/Recommendation.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
+
 import Button from "../Button/Button";
 import styles from "./Recommendation.module.css";
 
+interface PizzaOfDay {
+  title: string;
+  ingredients: string;
+  points: number;
+}
+
 export default function Recommendation() {
+  const [pizzaOfDay, setPizzaOfDay] = useState<PizzaOfDay>();
+
+  const getPizzaOfTheDay = async () => {
+    try {
+      await api
+        .get<PizzaOfDay>("/pizzaOfDay")
+        .then((res) => setPizzaOfDay(res.data));
+    } catch {
+      alert("Ocorreu um erro. Tente novamente mais tarde.");
+    }
+  };
+
+  useEffect(() => {
+    getPizzaOfTheDay();
+  }, []);
+
   return (
     <div className={styles.container}>
       <h2>Pizza do dia!</h2>
@@ -13,7 +37,7 @@ export default function Recommendation() {
         className={styles.pizzaImage}
       />
       <p className={styles.ingredients}>
-        Ingredientes: Calabresa, Queijo, Pimentão, Tomate e Azeitona.
+        Ingredientes: {pizzaOfDay?.ingredients}
       </p>
 
       <Button title="Selecionar" onClick={() => {}} />
