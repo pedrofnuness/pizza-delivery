ff94e341dfae49aa1bc790c8bf41d3047b8c3064
Pedro <pedro.nunes@southsystem.com.br>
2021-04-20 09:57:28 -0300

    :package: feat: connected Recommendation component to redux

--- frontend/src/components/Recommendation/Recommendation.tsx
+++ frontend/src/components/Recommendation/Recommendation.tsx
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import api from "../../services/api";
+import { saveItemToState } from "../../store/actions";
 
 import Button from "../Button/Button";
 import styles from "./Recommendation.module.css";
 
+interface RecommendationProps {
+  onSelect: () => void;
+}
 interface PizzaOfDay {
   title: string;
   ingredients: string;
   points: number;
 }
 
-export default function Recommendation() {
+export default function Recommendation({ onSelect }: RecommendationProps) {
+  const dispatch = useDispatch();
   const [pizzaOfDay, setPizzaOfDay] = useState<PizzaOfDay>();
 
   const getPizzaOfTheDay = async () => {
     try {
-      await api
-        .get<PizzaOfDay>("/pizzaOfDay")
-        .then((res) => setPizzaOfDay(res.data));
+      await api.get<PizzaOfDay>("/pizzaOfDay").then((res) => {
+        dispatch(saveItemToState("SET_RECOMMENDATION", res.data));
+        setPizzaOfDay(res.data);
+      });
     } catch {
       alert("Ocorreu um erro. Tente novamente mais tarde.");
     }
@@ -40,7 +47,7 @@ export default function Recommendation() {
         Ingredientes: {pizzaOfDay?.ingredients}
       </p>
 
-      <Button title="Selecionar" onClick={() => {}} />
+      <Button title="Selecionar" onClick={onSelect} />
     </div>
   );
 }
